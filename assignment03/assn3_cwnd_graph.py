5aee56b83624fa426106d3e393dc1ffe75904373
ohsj3781 <ohsj3781@gmail.com>
2025-06-12 18:53:39 +0000

    Change  assn3_cwnd_graph.py and Add log files

--- assignment03/assn3_cwnd_graph.py
+++ assignment03/assn3_cwnd_graph.py
@@ -11,8 +11,8 @@ for fn in file_names:
     with open(fn, "r") as f:
         lines = f.readlines()
         lines = [list(map(float, x.split())) for x in lines]
-        x_list = [x[0] for x in lines]
-        y_list = [x[1] for x in lines]
+        x_list = [x[1] for x in lines]
+        y_list = [x[0] for x in lines]
         data[fn] = [x_list, y_list]
 
 app = dash.Dash()  # initialising dash app
@@ -25,11 +25,11 @@ def gen_fig():
         objs.append(obj)
     fig = go.Figure(objs)
     fig.update_layout(xaxis_title='Time',
-                      yaxis_title='')
+                      yaxis_title='seq')
     return fig
 
 app.layout = html.Div(id='parent', children=[
-    html.H1(id='H1', children='TCP CWND 2026123123 내이름', style={'textAlign':'center',
+    html.H1(id='H1', children='Assn3 2020314916 오승재', style={'textAlign':'center',
                                                                  'marginTop':40, 'marginBottom':40}),
     dcc.Graph(id='line_plot', figure=gen_fig())
 ])
